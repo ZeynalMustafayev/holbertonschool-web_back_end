857e12aaa486bea3889f8443bf78aed84af94d6a
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-01 19:27:59 +0400

    fix pep error

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int, float]])->float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """sum_mixed_list"""
     sum = 0
     for i in mxd_lst:
