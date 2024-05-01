400e868e765adfa868ecc1c2ffc5cad295caf766
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-01 18:58:07 +0400

    add List

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,7 +2,10 @@
 """sum list"""
 
 
-def sum_list(input_list:list[float]) -> float:
+from typing import List
+
+
+def sum_list(input_list:List[float]) -> float:
     sum = 0
     for i in input_list:
         sum += i
