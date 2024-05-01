80bb1930d580df3d5c60ef4f50f984cc1a676f89
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-01 19:53:04 +0400

    fix pep8 error

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@
 from typing import Tuple, Union
 
 
-def to_kv(k : str, v : Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """to_kv"""
     v **= 2
     return k, v
