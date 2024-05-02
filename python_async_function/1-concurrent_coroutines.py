66debd01c06e77c9dee082b6ba8571af5ad6fb51
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-02 20:24:48 +0400

    sorted()

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """wait_n"""
     sort_list = []
     for _ in range(n):
         sort_list.append(await wait_random(max_delay))
-    return sort_list
+    return sorted(sort_list)
