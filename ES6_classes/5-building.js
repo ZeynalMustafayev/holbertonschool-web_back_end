d7a868c1b5722c66992dafec1f540aa090141602
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-19 01:50:40 +0400

    fix eslint error

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,14 +1,16 @@
 export default class Building {
-    constructor(sqft) {
-        this._sqft = sqft;
-        this.evacuationWarningMessage();
+  constructor(sqft) {
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    if (this.constructor !== Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    get sqft() {
-        return this._sqft;
-    }
-    evacuationWarningMessage() {
-        if (this.constructor !== Building) {
-            throw new Error("Class extending Building must override evacuationWarningMessage")
-        }
-    }
+  }
 }
