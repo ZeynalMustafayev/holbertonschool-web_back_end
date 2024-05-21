62062f82f6ba54b60cde921e290db88171c2ab5b
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-21 17:15:16 +0400

    fix eslint

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,8 +1,8 @@
 export default function signUpUser(firstName, lastName) {
-    return new Promise((resolved) => {
-        let success = true
-        if (success) {
-            resolved({firstName, lastName})
-        }
-    });
+  return new Promise((resolved) => {
+    const success = true;
+    if (success) {
+      resolved({ firstName, lastName });
+    }
+  });
 }
