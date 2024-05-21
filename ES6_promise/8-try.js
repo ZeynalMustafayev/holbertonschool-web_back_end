62474899fc72179369e06953ec62cae0b01b1269
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-21 20:53:52 +0400

    fixed

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,10 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-    return new Promise((resolve) => {
-        try {
-            resolve(numerator/denominator)
-        }
-        catch {
-            throw new Error('cannot divide by 0');
-        }
-    });
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return numerator / denominator;
 }
